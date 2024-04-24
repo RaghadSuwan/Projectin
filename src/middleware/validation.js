275e2e6d3990d8541c862cd2ab108068ca958dc3
rsuwan <r.r.suwan2001@gmail.com>
2024-04-24 09:00:51 +0300

    Validation

---
+++ src/middleware/validation.js
@@ -0,0 +1,43 @@
+import Joi from 'joi';
+export const generalFields = {
+    email: Joi.string().email().required().min(5).messages({
+        'string.empty': "Email is required",
+        'string.email': "Please enter a valid email"
+    }),
+
+    password: Joi.string().required().min(3).messages({
+        'string.empty': "Password is required",
+        'string.min': "Password should have a minimum length of {#limit} characters"
+    }),
+
+    file: Joi.object({
+        size: Joi.number().positive().required(),
+        path: Joi.string().required(),
+        filename: Joi.string().required(),
+        destination: Joi.string().required(),
+        mimetype: Joi.string().required(),
+        encoding: Joi.string().required(),
+        originalname: Joi.string().required(),
+        fieldname: Joi.string().required(),
+        dest: Joi.string(),
+    })
+};
+
+export const validation = (schema) => {
+    return (req, res, next) => {
+        const inputsData = { ...req.body, ...req.params, ...req.query};
+        if (req.file || req.files) {
+            inputsData.file = req.file || req.files;
+        }
+        const validationResult = schema.validate(inputsData, { abortEarly: false });
+        if (validationResult.error) {
+            return res.status(400).json({
+                message: "Validation error",
+                validationError: validationResult.error.details
+            });
+        }
+        next();
+
+    }
+};
+
--- src/modules/categories/categories.router.js
+++ src/modules/categories/categories.router.js
@@ -5,15 +5,18 @@ import subcategoryRouter from '../../modules/subcategory/subcategory.router.js';
 import { auth } from '../../middleware/auth.js';
 import { endPoint } from './categories.endPoint.js';
 import { asyncHandler } from '../../services/errorHanding.js';
+import { validation } from '../../middleware/validation.js';
+import * as validator from './categories.validation .js';
+
 const router = Router();
 router.use('/:id/subcategory', subcategoryRouter);//use: يعني اي اند بوينت
 router.get('/', auth(endPoint.getAll), asyncHandler(categoriesController.getCategories));
 router.get('/active', auth(endPoint.gatActive), asyncHandler(categoriesController.getactivecategories));
-router.get('/:id', auth(endPoint.specific), asyncHandler(categoriesController.specificcategory));
+router.get('/:id', auth(endPoint.specific), validation(validator.specificCategory), asyncHandler(categoriesController.specificcategory));
 router.post(
     '/', auth(endPoint.create),
-    fileUpload(fileValidation.image).single('image'),
-    asyncHandler(categoriesController.createcategory
+    fileUpload(fileValidation.image).single('image'), validation(validator.createCategory)
+    , asyncHandler(categoriesController.createcategory
     ));
 router.put(
     '/:id', auth(endPoint.update),
