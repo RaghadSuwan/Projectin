c985af9d2c5f1bfd47f8703c531aabb71cf800c5
rsuwan <r.r.suwan2001@gmail.com>
2024-05-07 14:01:42 +0300

    Pdf Update

--- src/utils/pdf.js
+++ src/utils/pdf.js
@@ -5,14 +5,14 @@ import { dirname, join } from "path";
 import { options } from "./options.js";
 const __fileName = fileURLToPath(import.meta.url)
 const __dirName = dirname(__fileName)
-
 export const createPdf = (users, fileName, req, res) => {
     const htmlPath = join(__dirName, "./../templets/pdf.html");
     let html = fs.readFileSync(htmlPath, "utf8");
     var document = {
         html: html,
-        data: { users },
+        data: {bill},
         path: `./${fileName}`,
     };
-    pdf.create(document, options).then(() => { return res.send(`<a download href="${req.protocol}://${req.headers.host}/${fileName}">download</a>`); });
+    pdf.create(document, options).then(() =>
+    { return res.send(`<a download href="${req.protocol}://${req.headers.host}/${fileName}">download</a>`); });
 };
