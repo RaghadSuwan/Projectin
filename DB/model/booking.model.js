067179c09f003fd2d50341559403a2c5bd6c84cd
rsuwan <r.r.suwan2001@gmail.com>
2024-05-25 14:42:55 +0300

    Booking db

--- DB/model/booking.model.js
+++ DB/model/booking.model.js
@@ -4,8 +4,8 @@ const bookingSchema = new Schema(
     {
         user_id: { type: Types.ObjectId, ref: "User", required: true },
         productId: { type: Types.ObjectId, ref: "Product", required: true },
-        start_date: { type: Date, required: true, required: true },
-        end_date: { type: Date, required: true, required: true },
+        start_date: { type: Date, required: true },
+        end_date: { type: Date ,required: true },
         return_date: { type: Date },
         status: {
             type: String,
